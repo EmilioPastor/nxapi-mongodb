42a60918ecc08a212d564232efc3e90c72566518
EmilioPastor <emiliopastorzurita906@gmail.com>
2025-02-14 16:54:19 +0100

    RECTIFICADO CORS

--- src/app/api/products/[id]/route.js
+++ src/app/api/products/[id]/route.js
@@ -1,28 +1,9 @@
-import Cors from 'cors';
+import { connectToDatabase } from "@/lib/mongodb";
+import { ObjectId } from "mongodb"; 
 
-// Configuración de CORS
-const cors = Cors({
-    methods: ['GET', 'PUT', 'DELETE'], // Métodos HTTP permitidos
-    origin: '*', // Permitir solicitudes desde cualquier origen. Cambia esto por un dominio específico si es necesario.
-});
-
-// Middleware para manejar CORS
-function runMiddleware(req, res, fn) {
-    return new Promise((resolve, reject) => {
-        fn(req, res, (result) => {
-            if (result instanceof Error) {
-                return reject(result); // Si hay un error, rechaza la promesa
-            }
-            return resolve(result); // Si todo va bien, resuelve la promesa
-        });
-    });
-}
 export async function GET(request, { params }) {
-    // Ejecutar CORS
-    await runMiddleware(request, {}, cors);
-
     const { database } = await connectToDatabase();
-    const collection = database.collection(process.env.MONGODB_COLLECTION);
+    const collection = database.collection("products");
 
     const { id } = params;
     const results = await collection.findOne({ _id: new ObjectId(id) });
@@ -31,9 +12,6 @@ export async function GET(request, { params }) {
 }
 
 export async function PUT(request, { params }) {
-    // Ejecutar CORS
-    await runMiddleware(request, {}, cors);
-
     const content = request.headers.get('content-type');
 
     if (content !== 'application/json') {
@@ -41,10 +19,10 @@ export async function PUT(request, { params }) {
     }
 
     const { database } = await connectToDatabase();
-    const collection = database.collection(process.env.MONGODB_COLLECTION);
+    const collection = database.collection("products");
 
     const { id } = params;
-    const { name, price, stock, description, category, image } = await request.json(); // Lee la solicitud
+    const { name, price, stock, description, category, image } = await request.json(); // Read body request
     const results = await collection.updateOne(
         { _id: new ObjectId(id) },
         { $set: { name, price, stock, description, category, image } }
@@ -54,11 +32,8 @@ export async function PUT(request, { params }) {
 }
 
 export async function DELETE(request, { params }) {
-    // Ejecutar CORS
-    await runMiddleware(request, {}, cors);
-
     const { database } = await connectToDatabase();
-    const collection = database.collection(process.env.MONGODB_COLLECTION);
+    const collection = database.collection("products");
 
     const { id } = params;
     const results = await collection.deleteOne({ _id: new ObjectId(id) });
